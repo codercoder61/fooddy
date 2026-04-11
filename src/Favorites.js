65fe8af9934ba84a2b3de3cae4eb1f5125d90b75
codercoder61 <mohamedknoud@gmail.com>
2026-04-11 13:16:03 +0200

    Update Favorites.js

--- src/Favorites.js
+++ src/Favorites.js
@@ -487,7 +487,7 @@ style={{ border: 'none', outline: 'none', height: '20px', width: '60px', }}
                 <h1 style={{fontSize:'1.3em'}}>Your Favorites 🤩</h1>
                 <div className="restaurants">
               {restaurants && restaurants.map((elm,index)=>(
-                <div key={index} style={{margin: '20px',width:'400px',aIndex:'10',position:'relative'}}>
+                <div key={index} style={{margin: '20px',width:'270px',aIndex:'10',position:'relative'}}>
                   <img alt="apap" width='100' height='100' style={{left:'50%',transform:'translate(-50%,-40%)',position:'absolute',objectFit:'cover',borderRadius:'50%'}} src={elm.logo!==null?`https://pneuexpress.online/foody/${elm.logo}`:'https://21985162c3f6de69b3a2fa38c4458a89.cdn.bubble.io/cdn-cgi/image/w=64,h=,f=auto,dpr=1,fit=contain/f1643645120337x435152378012765760/chefauchef-01%2B%281%29.png'}/>
                 <div  style={{margin:'20px',padding:'50px 0',display:'flex',flexDirection:'column',backgroundColor:'white',boxShadow:'0 0 2px #ddd',borderRadius:'15px'}}>
                   
